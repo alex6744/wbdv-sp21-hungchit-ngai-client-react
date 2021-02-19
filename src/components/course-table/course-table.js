cd5413bfbedec5da2fa3d6dff719232f141fcb81
alex6744 <alexngai6744@gmail.com>
2021-02-19 15:32:07 -0800

    add  tbody

--- src/components/course-table/course-table.js
+++ src/components/course-table/course-table.js
@@ -25,6 +25,9 @@ export default class CourseTable
                         </th>
 
                     </thead>
+                    <tbody>
+
+                    </tbody>
                 </table>
             </div>
         )
