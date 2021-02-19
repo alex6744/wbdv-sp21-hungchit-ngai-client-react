2ba63c3304427756661d5646461dd9fbb9bd984a
alex6744 <alexngai6744@gmail.com>
2021-02-19 14:45:14 -0800

    add  coursetble

---
+++ src/components/course-table/course-table.js
@@ -0,0 +1,32 @@
+
+import React from 'react'
+import CourseRow from "./course-row";
+import {Link} from "react-router-dom";
+
+
+export default class CourseTable
+    extends React.Component {
+
+
+
+    render() {
+        return(
+            <div>
+                <table class="table">
+                    <thead>
+                        <th >Title</th>
+                        <th >Owned by</th>
+                        <th >Last modified</th>
+                        <th >
+                            <i className="fas fa-2x fa-th float-right"></i>
+                            <i className="fas fa-2x fa-th float-right"></i>
+                            <i className="fas fa-2x fa-th float-right"></i>
+
+                        </th>
+
+                    </thead>
+                </table>
+            </div>
+        )
+    }
+}
