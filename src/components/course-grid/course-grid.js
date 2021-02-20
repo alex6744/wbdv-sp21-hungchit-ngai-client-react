267cab1ce1dd2bf3dbd908e2bf03a0dfc9dfa72d
alex6744 <alexngai6744@gmail.com>
2021-02-20 14:52:19 -0800

    added features

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -1,12 +1,39 @@
-
 import React from 'react'
-// import CourseCard from "./course-card";
 import {Link} from "react-router-dom";
 import CourseCard from "./course-card";
 
-const CourseGrid=()=>
-    <div>
-        <h1>CCCCC</h1>
-        <CourseCard/>
+const CourseGrid=({courses,deleteCourse})=>
+    <div className="container-fluid">
+
+        <div className="row">
+            <div className="col-5">
+                Recent Documents
+            </div>
+            <div className="col-3">
+                Owned by me
+                <i className="fas fa-sort-down fa-2x "></i>
+
+
+            </div>
+            <div className="col-4">
+                <i className="fas fa-folder fa-2x "></i>
+                <i className="fas fa-sort fa-2x "></i>
+                <Link to="/courses/table">
+                    <i className="fas fa-list fa-2x float-right"></i>
+                </Link>
+
+            </div>
+        </div>
+        <div className="row">
+            {
+               courses.map(course=>
+                   <CourseCard
+                       course={course}
+                       deleteCourse={deleteCourse}
+                   />
+               )
+            }
+        </div>
+
     </div>
 export default CourseGrid
