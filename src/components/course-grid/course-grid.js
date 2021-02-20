83e4a4016661d68f167909cd25e21ccd82feac38
alex6744 <alexngai6744@gmail.com>
2021-02-20 15:31:32 -0800

    add detail to col

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import CourseCard from "./course-card";
+import "./course-grid.css"
 
 const CourseGrid=({courses,deleteCourse})=>
     <div className="container-fluid">
 
         <div className="row">
-            <div className="col-5">
+            <div className="col-5 d-none d-lg-block">
                 Recent Documents
             </div>
-            <div className="col-3">
+            <div className="col-3 d-none d-lg-block">
                 Owned by me
                 <i className="fas fa-sort-down fa-2x "></i>
 
 
             </div>
-            <div className="col-4">
-                <i className="fas fa-folder fa-2x "></i>
-                <i className="fas fa-sort fa-2x "></i>
+
+            <div className="col-4 ">
+                <i className="fas fa-folder fa-2x position-floder"></i>
+                <i className="fas fa-sort fa-2x position-sort "></i>
                 <Link to="/courses/table">
-                    <i className="fas fa-list fa-2x float-right"></i>
+                    <i className="fas fa-list fa-2x position-grid"></i>
                 </Link>
 
             </div>
