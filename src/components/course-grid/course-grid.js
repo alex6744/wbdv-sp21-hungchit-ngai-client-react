82aea33d7678fbae65a04a69f5bd633aeba50fbd
alex6744 <alexngai6744@gmail.com>
2021-02-19 20:49:17 -0800

    added function

---
+++ src/components/course-grid/course-grid.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+// import CourseCard from "./course-card";
+import {Link} from "react-router-dom";
+
+const CourseGrid=()=>
+    <div>
+
+    </div>
+export default CourseGrid
