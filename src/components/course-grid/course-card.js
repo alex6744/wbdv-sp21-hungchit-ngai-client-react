2c8b681955ce9fa0289eedfa2d8944b571c69acb
alex6744 <alexngai6744@gmail.com>
2021-02-20 14:52:02 -0800

    added title, parameters

--- src/components/course-grid/course-card.js
+++ src/components/course-grid/course-card.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 
-const CourseCard=()=>
+const CourseCard=({course,deleteCourse})=>
     <div className="col-4">
         <div className="card">
             <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png" className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">1111111111</h5>
+                <h5 className="card-title">{course.title}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                     content.</p>
                 <img src={``}/>
                 <Link to="/courses/editor" className="btn btn-primary">
-                    111111
+                    {course.title}
                 </Link>
-                <i className="fas fa-trash"></i>
+                <i onClick={()=>deleteCourse(course)} className="fas fa-trash"></i>
             </div>
         </div>
     </div>
