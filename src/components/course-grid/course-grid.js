2bfb3285d994ba4a5300b27771b6409940fb3529
alex6744 <alexngai6744@gmail.com>
2021-02-19 23:59:45 -0800

    add title

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -2,9 +2,11 @@
 import React from 'react'
 // import CourseCard from "./course-card";
 import {Link} from "react-router-dom";
+import CourseCard from "./course-card";
 
 const CourseGrid=()=>
     <div>
-
+        <h1>CCCCC</h1>
+        <CourseCard/>
     </div>
 export default CourseGrid
