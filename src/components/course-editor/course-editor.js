26749209fa524014200b8091cbe1c1c0176a50dd
alex6744 <alexngai6744@gmail.com>
2021-02-20 00:49:20 -0800

    add features

---
+++ src/components/course-editor/course-editor.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+const CourseEditor =()=>
+    <div>
+        <h2>
+            <Link  to="/courses/table">
+                <i className="fas fa-arrow-left"></i>
+            </Link>
+            Course Editor
+            {/*<i onClick={()=>props.history.goBack()}*/}
+            {/*   className="fas fa-times float-right"></i>*/}
+        </h2>
+    </div>
+export default CourseEditor
