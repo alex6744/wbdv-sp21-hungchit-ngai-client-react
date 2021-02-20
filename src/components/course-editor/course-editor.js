c8beb39cf8c8c8e324e4e2a9533f7da44279fc4c
alex6744 <alexngai6744@gmail.com>
2021-02-20 12:57:04 -0800

    added editor page from prev assignment

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+
 const CourseEditor =()=>
-    <div>
+    <div className="container-fluid">
         <h2>
             <Link  to="/courses/table">
                 <i className="fas fa-arrow-left"></i>
@@ -10,5 +11,136 @@ const CourseEditor =()=>
             {/*<i onClick={()=>props.history.goBack()}*/}
             {/*   className="fas fa-times float-right"></i>*/}
         </h2>
+        <div>
+            <div className="row ">
+                <div className="col-1">
+                    <a href="../index.html">
+                        <i className=" fa fa-times fa-2x color-white"></i>
+                    </a>
+                </div>
+                <div className="col-3 coursename-position ">
+
+
+                    <h4 className="color-white ">CS5610-WebDev</h4>
+
+                </div>
+                <div className="col-7">
+                    <ul className="nav nav-pills ">
+                        <li className="nav-item ">
+                            <a className="nav-link  color-white" aria-current="page" href="#">
+                                Build
+                            </a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link active color-white " href="#">
+
+                                Pages
+
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link color-white" href="#">
+                                Theme
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link color-white" href="#">
+                                Store
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link color-white" href="#">
+                                Apps
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link color-white" href="#">
+                                Setting
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link " href="#" tabIndex="-1" aria-disabled="true">
+                                <i className="fa fa-plus color-white"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-4">
+                <br/>
+                <ul className="list-group">
+                    <li className="list-group-item ">
+                        Module 1-JQuery
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+                    <li className="list-group-item active">
+                        Module 2-React
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+                    <li className="list-group-item">
+                        Module 3-Redux
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+                    <li className="list-group-item">
+                        Module 4-Native
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+                    <li className="list-group-item">
+                        Module 5-Angular
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+                    <li className="list-group-item">
+                        Module 6-Node
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+                    <li className="list-group-item">
+                        Module 7-Mongo
+                        <i className="pull-right fa fa-trash"></i>
+                    </li>
+
+                </ul>
+                <div className="trash-position">
+                    <i className="pull-right fa fa-plus fa-2x"></i>
+                </div>
+
+            </div>
+
+            <div className="col-8">
+
+                <br/>
+
+                <ul className="nav nav-pills">
+                    <li className="nav-item">
+                        <a className="nav-link " aria-current="page" href="#">
+                            Topic 1
+                        </a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link active" href="#">
+                            Topic 2
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            Topic 3
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            Topic 4
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link " href="#" tabIndex="-1" aria-disabled="true">
+                            <i className="fa fa-plus"></i>
+                        </a>
+                    </li>
+                </ul>
+                <h1>Blank</h1>
+            </div>
+        </div>
     </div>
 export default CourseEditor
