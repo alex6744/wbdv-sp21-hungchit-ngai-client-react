814cd42d3ae139dd9e4901f26b79eb72adb71d03
alex6744 <alexngai6744@gmail.com>
2021-02-22 09:52:18 -0800

    added icons and features

--- src/components/course-grid/course-card.js
+++ src/components/course-grid/course-card.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
+import "./course-grid.css"
+const CourseCard=({course,deleteCourse,updateCourse})=>{
+    const [editing, setEditing] = useState(false)
+    const [newTitle, setNewTitle] = useState(course.title)
 
-const CourseCard=({course,deleteCourse})=>
+    const saveTitle = () => {
+        setEditing(false)
+        const newCourse = {
+            ...course,
+            title: newTitle
+        }
+        updateCourse(newCourse)
+    }
+
+    return(
     <div className="col-4">
         <div className="card">
             <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png" className="card-img-top" alt="..."/>
@@ -10,11 +23,31 @@ const CourseCard=({course,deleteCourse})=>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                     content.</p>
                 <img src={``}/>
-                <Link to="/courses/editor" className="btn btn-primary">
-                    {course.title}
-                </Link>
-                <i onClick={()=>deleteCourse(course)} className="fas fa-trash"></i>
+                {
+                    !editing &&
+                    <Link to="/courses/editor" className="btn btn-primary">
+                        {course.title}
+                    </Link>
+                }
+                {
+                    editing &&
+                    <input className="form-control"
+                           onChange={(event) =>{ setNewTitle(event.target.value)}}
+                           value={newTitle}
+                            />
+                }
+                {/*<i onClick={()=>deleteCourse(course)} className="fas fa-trash"></i>*/}
+                {!editing && <i className="fas fa-edit float-right 	d-none d-lg-block d-xl-none"
+                    onClick={()=>setEditing(true)}></i>}
             </div>
+            {editing && <i onClick={() => saveTitle()}
+                            className="fas fa-check fa-2x position-check"></i>}
+            {editing && <i className="fas fa-times fa-2x position-time"
+               onClick={()=>{ setEditing(false)
+                    deleteCourse(course)}}
+               title="delete Course"></i>}
         </div>
-    </div>
+
+    </div>)
+}
 export default CourseCard
