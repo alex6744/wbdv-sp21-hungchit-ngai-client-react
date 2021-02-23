b0a46f10fe01fea0381cd642ef408da044c8f550
alex6744 <alexngai6744@gmail.com>
2021-02-22 17:45:57 -0800

    coloumn resize

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -4,24 +4,35 @@ import CourseCard from "./course-card";
 import "./course-grid.css"
 
 const CourseGrid=({courses,deleteCourse,updateCourse})=>
-    <div className="container-fluid">
+    <div className="container-fluid" >
 
         <div className="row">
             <div className="col-5 d-none d-md-block">
                 Recent Documents
             </div>
-            <div className="col-3 d-none d-md-block">
+            <div className="col-4 d-none d-md-block">
                 Owned by me
                 <i className="fas fa-sort-down fa-2x "></i>
 
 
             </div>
 
-            <div className="col-4 ">
+            <div className="col-1 col-xs-3  	">
+
                 <i className="fas fa-folder fa-2x position-floder"></i>
+
+            </div>
+            <div className="col-1 col-xs-3	">
+
+
                 <i className="fas fa-sort fa-2x position-sort "></i>
+            </div>
+
+            <div className="col-1 col-xs-3 " >
+
+
                 <Link to="/courses/table">
-                    <i className="fas fa-list fa-2x position-grid"></i>
+                    <i className="fas fa-list fa-2x  position-grid"></i>
                 </Link>
 
             </div>
