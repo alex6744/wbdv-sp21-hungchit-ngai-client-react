dc8fe18cda84a9fce521ac1042341f77e15464dd
alex6744 <alexngai6744@gmail.com>
2021-03-04 16:16:36 -0800

    added methods

---
+++ src/services/module-service.js
@@ -0,0 +1,21 @@
+const COURSES_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/courses/";
+const MODULES_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/modules/";
+
+export const creatModule=(courseId,module)=>{
+
+}
+
+export const findModulesForCourse=(courseId)=>{
+
+}
+export const updateModule=(moduleId, module)=>{
+
+}
+export const deleteModule=(moduleId)=>{
+
+}
+
+const api={
+    creatModule,findModulesForCourse,updateModule,deleteModule
+};
+export default api;
