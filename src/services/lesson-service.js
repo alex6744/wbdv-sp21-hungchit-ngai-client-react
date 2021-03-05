75b11f5ea0c856cb8e5079ff8367b4ce7b8aa993
alex6744 <alexngai6744@gmail.com>
2021-03-04 17:11:41 -0800

    added methods

---
+++ src/services/lesson-service.js
@@ -0,0 +1,22 @@
+const LESSONS_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/lessons/";
+const MODULES_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/modules/";
+
+export const createLesson=(moduleId,lesson)=>{
+
+}
+
+export const findLessonsForModule=(moduleId)=>{
+
+}
+
+export const updateLesson=(lessonId,lesson)=>{
+
+}
+
+export const deleteLesson=(lessonId)=>{
+
+}
+
+export default {
+    createLesson,findLessonsForModule,updateLesson,deleteLesson
+}
---
+++ src/services/topic-service.js
@@ -0,0 +1,22 @@
+const LESSONS_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/lessons/";
+const TOPIC_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/topics/";
+
+export const createTopic=(lessonId,topic)=>{
+
+}
+
+export const findTopicsForLesson=(lessonId)=>{
+
+}
+
+export const updateTopic=(topicId,topic)=>{
+
+}
+
+export const deleteTopic=(topicId)=>{
+
+}
+
+export default {
+    createTopic,findTopicsForLesson,updateTopic,deleteTopic
+}
