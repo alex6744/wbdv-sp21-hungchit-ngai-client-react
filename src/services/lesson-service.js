cabb15712ced99284f659c93ecc52b4a4b59b0ce
alex6744 <alexngai6744@gmail.com>
2021-03-09 15:05:42 -0800

    add feature to method

--- src/services/lesson-service.js
+++ src/services/lesson-service.js
@@ -1,21 +1,39 @@
 const LESSONS_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/lessons/";
 const MODULES_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/modules/";
 
-export const createLesson=(moduleId,lesson)=>{
+export const createLesson=(moduleId,lesson)=>
+    fetch(`${MODULES_URL}/${moduleId}/lessons`, {
+        method: "POST",
+        body: JSON.stringify(lesson),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
 
-}
+export const findLessonsForModule=(moduleId)=>
+    fetch(`${MODULES_URL}/${moduleId}/lessons`)
+        .then(response => response.json())
 
-export const findLessonsForModule=(moduleId)=>{
+export const updateLesson=(lessonId,lesson)=>
+    fetch(`${LESSONS_URL}/${lessonId}`, {
+        method: "PUT",
+        body: JSON.stringify(lesson),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
 
-}
 
-export const updateLesson=(lessonId,lesson)=>{
 
-}
+export const deleteLesson=(lessonId)=>
+    fetch(`${LESSONS_URL}/${lessonId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
 
-export const deleteLesson=(lessonId)=>{
 
-}
 
 export default {
     createLesson,findLessonsForModule,updateLesson,deleteLesson
--- src/services/topic-service.js
+++ src/services/topic-service.js
@@ -1,21 +1,41 @@
 const LESSONS_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/lessons/";
 const TOPIC_URL="https://wbdv-generic-server.herokuapp.com/api/001501828/topics/";
 
-export const createTopic=(lessonId,topic)=>{
+export const createTopic=(lessonId,topic)=>
+    fetch(`${LESSONS_URL}/${lessonId}/topics`, {
+        method: "POST",
+        body: JSON.stringify(topic),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
 
-}
 
-export const findTopicsForLesson=(lessonId)=>{
+export const findTopicsForLesson=(lessonId)=>
+    fetch(`${LESSONS_URL}/${lessonId}/topics`)
+        .then(response => response.json())
 
-}
 
-export const updateTopic=(topicId,topic)=>{
+export const updateTopic=(topicId,topic)=>
+    fetch(`${TOPIC_URL}/${topicId}`, {
+        method: "PUT",
+        body: JSON.stringify(topic),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
 
-}
 
-export const deleteTopic=(topicId)=>{
 
-}
+export const deleteTopic=(topicId)=>
+    fetch(`${TOPIC_URL}/${topicId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+
 
 export default {
     createTopic,findTopicsForLesson,updateTopic,deleteTopic
