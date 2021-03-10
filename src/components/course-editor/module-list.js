18226cf32c2e70d1071f9f6338b9b7b0951bd397
alex6744 <alexngai6744@gmail.com>
2021-03-09 19:20:20 -0800

    add features

--- src/components/course-editor/module-list.js
+++ src/components/course-editor/module-list.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {connect} from 'react-redux'
+import EditableItem from "../editable-item";
+import {useParams} from "react-router-dom";
+import moduleService from "../../services/module-service";
 
-const ModuleList=()=>{
 
+const ModuleList=(
+    {
+
+
+
+    })=>{
+    const {courseId,moduleId}=useParams()
+    useEffect(()=>{
+
+    })
     return(
         <div>
             <h2>Modules</h2>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <EditableItem/>
+                </li>
+                <li className="list-group-item">
+                    <i  className="fas fa-plus fa-2x"></i>
+                </li>
+            </ul>
+
         </div>
     )
 }
@@ -13,8 +34,12 @@ const ModuleList=()=>{
 const stpm=()=>{
 
 }
-const dtpm=()=>{
+const dtpm=(dispatch)=>{
+    return{
+        creatModule:()=>{
 
+        },
+    }
 }
 
 export default connect(stpm,dtpm)(ModuleList)
