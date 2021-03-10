121a4490b8777c6be765547581d1d8adbde7dee5
alex6744 <alexngai6744@gmail.com>
2021-03-10 07:42:13 -0800

    add features

--- src/components/editable-item.js
+++ src/components/editable-item.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
 
-const EditableItem=()=>{
-
+const EditableItem = (
+    {
+        to,
+        item,
+        updateItem,
+        deleteItem
+    }) => {
+    const [editing, setEditing] = useState(false)
+    const [itemCache, setItemCache] = useState(item)
     return(
         <>
-            <input/>
-            <i className="fas fa-check"></i>
-            <i className="fas fa-times"></i>
+            {
+                !editing &&
+                <>
+                    <Link to={to}>
+                        {item.title}
+                    </Link>
+                    <i onClick={() => setEditing(true)} className="fas fa-edit"></i>
+                </>
+            }
+            {
+                editing &&
+                <>
+                    <input
+                        onChange={(e) => setItemCache({...itemCache, title: e.target.value})}
+                        value={itemCache.title}/>
+                    <i onClick={() => {
+                        setEditing(false)
+                        updateItem(itemCache)
+                    }} className="fas fa-check"></i>
+                    <i onClick={() => deleteItem(item)} className="fas fa-times"></i>
+                </>
+            }
         </>
     )
 }
+
 export default EditableItem
