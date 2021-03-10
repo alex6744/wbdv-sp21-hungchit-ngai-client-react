66bfe21026f6c599188af21b428cc45e9fc3ad18
alex6744 <alexngai6744@gmail.com>
2021-03-09 19:19:47 -0800

    add details

---
+++ src/components/editable-item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EditableItem=()=>{
+
+    return(
+        <>
+            <input/>
+            <i className="fas fa-check"></i>
+            <i className="fas fa-times"></i>
+        </>
+    )
+}
+export default EditableItem
