2d7aa646287f787cb1682c7e24f4e7efdef6da3b
alex6744 <alexngai6744@gmail.com>
2021-03-20 12:10:02 -0700

    created widgetservice

---
+++ src/services/widget-service.js
@@ -0,0 +1,43 @@
+const WIDGET_URL="http://localhost:8080/api/widgets";
+const TOPIC_URL="http://localhost:8080/api/topics";
+
+
+export const createWidget=(tid,widget)=>
+    fetch(`${TOPIC_URL}/${tid}/widgets`, {
+        method: "POST",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+
+export const findWidgetsForTopic=(tid)=>
+    fetch(`${TOPIC_URL}/${tid}/widgets`)
+        .then(response => response.json())
+
+
+export const updateWidget=(wid, widget)=>
+    fetch(`${WIDGET_URL}/${wid}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+
+
+export const deleteWidget=(wid)=>
+    fetch(`${WIDGET_URL}/${wid}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+
+
+export default {
+    createTopic,findTopicsForLesson,updateTopic,deleteTopic
+}
