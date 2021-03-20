6b1383bd7575388fa9807892c0c006eea1ceaaa4
alex6744 <alexngai6744@gmail.com>
2021-03-20 05:53:09 -0700

    create new widget part

---
+++ src/components/widgets/heading-widget.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const HeadingWidget=()=>
+    <div>
+
+    </div>
+
+export default HeadingWidget
---
+++ src/components/widgets/paragraph-widget.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react'
+
+const ParagraphWidget=()=>
+    <div>
+
+    </div>
+
+export default ParagraphWidget
