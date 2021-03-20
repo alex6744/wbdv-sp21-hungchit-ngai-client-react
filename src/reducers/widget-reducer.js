ea14005da9d6a3a66b014f9ffd3bd0a722ce500e
alex6744 <alexngai6744@gmail.com>
2021-03-20 05:53:25 -0700

    created widget reducer

--- src/reducers/widget-reducer.js
+++ src/reducers/widget-reducer.js
@@ -6,41 +6,20 @@ const intitialState={
 
 const widgetReducer =(state=intitialState,action)=>{
     switch (action.type){
-        case "CREATE_TOPIC":
+        case "CREATE_WIDGET":
             return {
                 ...state,
-                topics: [
-                    ...state.topics,
-                    action.topic
+                widgets: [
+                    ...state.widgets,
+                    action.widget
                 ]
             }
-        case "FIND_TOPICS_FOR_LESSON":
+        case "FIND_ALL_WIDGETS_FOR_TOPIC":
             return {
                 ...state,
-                topics: action.topics
+                widgets: action.widgets
             }
-        case "UPDATE_TOPIC":
-            return {
-                ...state,
-                topics: state.topics.map(topic => {
-                    if(topic._id === action.updateTopic._id) {
-                        return action.updateTopic
-                    } else {
-                        return topic
-                    }
-                })
-            }
-        case "DELETE_TOPIC":
-            return {
-                ...state,
-                topics:state.topics.filter(topic=>{
-                    if(topic._id !== action.topicToDelete._id){
-                        return true
-                    }else {
-                        return false
-                    }
-                })
-            }
+
         default:
             return state
     }
