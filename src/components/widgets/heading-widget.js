d93a1fdd8686e1f805ec14cffab0ffc66e9a4699
alex6744 <alexngai6744@gmail.com>
2021-03-21 08:54:54 -0700

    add elements

--- src/components/widgets/heading-widget.js
+++ src/components/widgets/heading-widget.js
@@ -1,32 +1,63 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const HeadingWidget=({widget, setWidget, editing})=>
-    <div>
-        <ul className="list-group">
-            <li className="list-group-item">Heading</li>
-            <li className="list-group-item">
-                <div>
-                    <select  className="form-control">
-                        <option value={1}>Heading</option>
-                        <option value={2}>Paragraph</option>
-                        <option value={3}>Video</option>
-                        <option value={4}>Image</option>
-                        <option value={5}>Link</option>
-                        <option value={6}>List</option>
-                        <option value={6}>HTML</option>
-                    </select>
-                    <input className="form-control"/>
-                    <select  className="form-control">
-                        <option value={1}>Heading 1</option>
-                        <option value={2}>Heading 2</option>
-                        <option value={3}>Heading 3</option>
-                        <option value={4}>Heading 4</option>
-                        <option value={5}>Heading 5</option>
-                        <option value={6}>Heading 6</option>
-                    </select>
-                </div>
-            </li>
-        </ul>
-    </div>
+const HeadingWidget=({widget,updateWidget,deleteWidget})=> {
+    const [itemCache, setItemCache] = useState(widget)
+    const [editing,setEditing]=useState(false)
+    return(
+        <div>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <h1>Heading {widget.type}</h1>
+                    <i className="fas fa-cog float-right"
+                        onClick={()=>setEditing(true)}></i>
+                </li>
+
+                {
+                    editing &&
+                    <li className="list-group-item">
+
+                        <div>
+
+
+                            <>
+                                <i className="fas fa-check float-right"
+                                    onClick={()=>{
+                                        setEditing(false)
+                                        updateWidget(itemCache)
+                                    }}></i>
+                                <i className="fas fa-trash float-right"
+                                    onClick={()=>{}}></i>
+                            </>
+
+
+                            <select className="form-control"
+                                    onChange={(e) =>setItemCache({...itemCache, type: e.target.value})}>
+                                <option value="HEADING">Heading</option>
+                                <option value="PARAGRAPH">Paragraph</option>
+                                <option value="VIDEO">Video</option>
+                                <option value="Image">Image</option>
+                                <option value="LINK">Link</option>
+                                <option value="LIST">List</option>
+                                <option value="HTML">HTML</option>
+                            </select>
+                            <input className="form-control"/>
+                            <select className="form-control">
+                                <option value={1}>Heading 1</option>
+                                <option value={2}>Heading 2</option>
+                                <option value={3}>Heading 3</option>
+                                <option value={4}>Heading 4</option>
+                                <option value={5}>Heading 5</option>
+                                <option value={6}>Heading 6</option>
+                            </select>
+
+                        </div>
+
+
+                    </li>
+                }
+            </ul>
+        </div>
+    )
+}
 
 export default HeadingWidget
