429964dd68ec101ccb9908c91dbd4bde9aa23b0c
alex6744 <alexngai6744@gmail.com>
2021-03-21 12:46:57 -0700

    change url

--- src/services/widget-service.js
+++ src/services/widget-service.js
@@ -1,5 +1,5 @@
-const WIDGET_URL="http://localhost:8080/api/widgets";
-const TOPIC_URL="http://localhost:8080/api/topics";
+const WIDGET_URL="https://hungchit-ngai.herokuapp.com/api/widgets";
+const TOPIC_URL="https://hungchit-ngai.herokuapp.com/api/topics";
 
 
 export const createWidget=(tid,widget)=>
