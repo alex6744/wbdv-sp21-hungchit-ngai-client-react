67d0a4f2d0c47b76a8f5218ad6a3a8b78f90ae00
alex6744 <alexngai6744@gmail.com>
2021-03-20 18:04:29 -0700

    add new methods

--- src/reducers/widget-reducer.js
+++ src/reducers/widget-reducer.js
@@ -19,7 +19,28 @@ const widgetReducer =(state=intitialState,action)=>{
                 ...state,
                 widgets: action.widgets
             }
-
+        case "UPDATE_WIDGET":
+            return {
+                ...state,
+                widgets: state.widgets.map(widget => {
+                    if(widget.id === action.updateWidget.id) {
+                        return action.updateWidget
+                    } else {
+                        return widget
+                    }
+                })
+            }
+        case "DELETE_WIDGET":
+            return {
+                ...state,
+                widgets:state.widgets.filter(widget=>{
+                    if(widget.id !== action.widgetToDelete.id){
+                        return true
+                    }else {
+                        return false
+                    }
+                })
+            }
         default:
             return state
     }
