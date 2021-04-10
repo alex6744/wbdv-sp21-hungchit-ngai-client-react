328c593d7dd9059685935fef237b45bdb5b8fbaf
alex6744 <alexngai6744@gmail.com>
2021-04-10 16:41:41 -0700

    change url

--- src/services/widget-service.js
+++ src/services/widget-service.js
@@ -1,8 +1,8 @@
-// const WIDGET_URL="https://hungchit-ngai.herokuapp.com/api/widgets";
-// const TOPIC_URL="https://hungchit-ngai.herokuapp.com/api/topics";
+const WIDGET_URL="https://hungchit-ngai.herokuapp.com/api/widgets";
+const TOPIC_URL="https://hungchit-ngai.herokuapp.com/api/topics";
 
-const WIDGET_URL="http://localhost:8080/api/widgets";
-const TOPIC_URL="http://localhost:8080/api/topics";
+// const WIDGET_URL="http://localhost:8080/api/widgets";
+// const TOPIC_URL="http://localhost:8080/api/topics";
 
 
 export const createWidget=(tid,widget)=>
