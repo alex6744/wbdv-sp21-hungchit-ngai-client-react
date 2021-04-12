816e1e453bffe5fbce967f7ed48f5bd67e98f870
alex6744 <alexngai6744@gmail.com>
2021-04-11 18:19:05 -0700

    add new routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import {BrowserRouter,Route} from "react-router-dom";
 import Home from "./components/home"
 import CourseEditor from "./components/course-editor/course-editor";
+import QuizzesList from "./components/quizzes/quizzes-list";
+import Quiz from "./components/quizzes/quiz";
 
 
 function App() {
@@ -26,6 +28,12 @@ function App() {
                        exact={true}
                        render={(props) => <CourseEditor {...props}/>}>
 
+                </Route>
+                <Route path="/courses/:courseId/quizzes" exact={true}>
+                    <QuizzesList/>
+                </Route>
+                <Route path="/courses/:courseId/quizzes/:quizId" exact={true}>
+                    <Quiz/>
                 </Route>
             </div>
         </BrowserRouter>
