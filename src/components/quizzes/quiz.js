d8f6c32e7cfafa6842eb5070158409d3d00d7e49
alex6744 <alexngai6744@gmail.com>
2021-04-19 07:44:58 -0700

    add submit button

--- src/components/quizzes/quiz.js
+++ src/components/quizzes/quiz.js
@@ -8,6 +8,11 @@ const Quiz=()=>{
     const [questions,setQuestions]=useState([])
     const [quiz,setQuiz]=useState({})
     const {courseId,quizId}=useParams();
+    const [enable,setEnable]=useState(false)
+    const [attempt,setAttempt]=useState([])
+    const [result,setResult]=useState({})
+
+
     useEffect(()=>{
         questionService.findQuestionsForQuiz(quizId)
             .then(questions=>setQuestions(questions))
@@ -28,13 +33,35 @@ const Quiz=()=>{
 
                     questions.map(question=>
                         <div className="list-group-item">
-                            <Question question={question}/>
+                            <Question question={question}
+                                        attempt={attempt}
+                                        setAttempt={setAttempt}/>
                         </div>
 
 
                     )
                 }
             </div>
+            <br/>
+            <button className="float-right btn btn-success"
+                    disabled={`${enable?'disabled':''}`}
+                    onClick={()=> {
+                        if(attempt.length===questions.length){
+                            setEnable(true)
+                            quizService.submitQuiz(quizId,attempt).then(result=>setResult(result))
+
+                        }else{
+                            alert("please answer all questions")
+                        }
+
+                    }}>Get your score</button>
+            Score:
+            <br/>
+            {
+                enable&&
+                  result.score
+            }
+
         </div>
     )
 }
