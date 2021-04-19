66211567299d8e99a70f8b3b444b759dc4a48c59
alex6744 <alexngai6744@gmail.com>
2021-04-19 07:45:45 -0700

    add parameters

--- src/components/quizzes/questions/question.js
+++ src/components/quizzes/questions/question.js
@@ -2,16 +2,20 @@ import React from "react";
 import TrueFalseQuestion from "./true-false-question";
 import MultipleChoiceQuestion from "./multiple-choice-question";
 
-const Question=({question})=>{
+const Question=({question,attempt,setAttempt})=>{
     return(
         <div>
             {
                 question.type==="TRUE_FALSE"&&
-                    <TrueFalseQuestion question={question}/>
+                    <TrueFalseQuestion question={question}
+                                        attempt={attempt}
+                                       setAttempt={setAttempt}/>
             }
             {
                 question.type==="MULTIPLE_CHOICE"&&
-                    <MultipleChoiceQuestion question={question}/>
+                    <MultipleChoiceQuestion question={question}
+                                            attempt={attempt}
+                                            setAttempt={setAttempt}/>
             }
         </div>
     )
