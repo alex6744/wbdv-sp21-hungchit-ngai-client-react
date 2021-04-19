c9c92138aabec825d03915dc2bcfc8ac224a0bbf
alex6744 <alexngai6744@gmail.com>
2021-04-19 07:44:14 -0700

    add new method

--- src/services/quizzes-service.js
+++ src/services/quizzes-service.js
@@ -1,5 +1,5 @@
-// const QUIZZES_URL = 'http://localhost:3000/api/quizzes';
-const QUIZZES_URL ='https://hungchit-ngai-node.herokuapp.com/api/quizzes';
+const QUIZZES_URL = 'http://localhost:3000/api/quizzes';
+//const QUIZZES_URL ='https://hungchit-ngai-node.herokuapp.com/api/quizzes';
 const findAllQuizzes = () => {
     return fetch(QUIZZES_URL)
         .then(response => response.json())
@@ -8,6 +8,18 @@ const findQuizById = (qid) => {
     return fetch(`${QUIZZES_URL}/${qid}`)
         .then(response => response.json())
 }
+const submitQuiz = (quizId, questions) => {
+   return  fetch(`${QUIZZES_URL}/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+
+}
+const findAttempts=(qid)=>fetch(`${QUIZZES_URL}/${qid}/attempts`).then(response => response.json())
+
 export default {
-    findAllQuizzes, findQuizById
+    findAllQuizzes, findQuizById,submitQuiz,findAttempts
 }
